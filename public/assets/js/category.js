a3f4b6d3f4d855252aac5f8942bb2aa8e517959c
ifer-itcast <yangkangkang@itcast.cn>
2019-12-17 20:34:05 +0800

    分类列表

--- public/assets/js/category.js
+++ public/assets/js/category.js
@@ -1,3 +1,4 @@
+// 添加分类
 $('#addCategory').on('submit', function() {
     let formData = $(this).serialize();
     $.ajax({
@@ -9,4 +10,16 @@ $('#addCategory').on('submit', function() {
         }
     });
     return false;
+});
+
+// 分类列表
+$.ajax({
+    type: 'GET',
+    url: '/categories',
+    success: function (response) {
+        let html = template('categoryListTpl', {
+            data: response
+        });
+        $('#categoryBox').html(html);
+    }
 });
