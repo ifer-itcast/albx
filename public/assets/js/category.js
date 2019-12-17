c5818b49b208d93390a0ca2be7951a08e1a70cd1
ifer-itcast <yangkangkang@itcast.cn>
2019-12-17 20:55:36 +0800

    分类修改

--- public/assets/js/category.js
+++ public/assets/js/category.js
@@ -23,3 +23,33 @@ $.ajax({
         $('#categoryBox').html(html);
     }
 });
+
+
+// 分类修改页面
+$('#categoryBox').on('click', '.edit', function() {
+    let id = $(this).attr('data-id');
+    // 根据 id 获取分类数据的详细信息
+    $.ajax({
+        type: 'GET',
+        url: '/categories/'+id,
+        success: function (response) {
+            let html = template('modifyCategoryTpl', response);
+            $('#formBox').html(html);
+        }
+    });
+});
+
+// 分类修改功能
+$('#formBox').on('submit', '#modifyCategory', function() {
+    let formData = $(this).serialize();
+    let id = $(this).attr('data-id');
+    $.ajax({
+        type: 'PUT',
+        url: '/categories/' + id,
+        data: formData,
+        success: function () {
+            location.reload();
+        }
+    });
+    return false;
+});
