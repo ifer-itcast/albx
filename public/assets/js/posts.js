ffd9f3a43deb149a3b317d0071ebadb72402924d
ifer-itcast <yangkangkang@itcast.cn>
2019-12-18 17:57:06 +0800

    文章列表分页

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -4,6 +4,9 @@ $.ajax({
     success: function (response) {
         let html = template('postsTpl', response);
         $('#postsBox').html(html);
+        
+        let page = template('pageTpl', response);
+        $('#page').html(page);
     }
 });
 
@@ -11,4 +14,22 @@ function formatDate(date) {
     // 将日期时间字符串转日期对象
     date = new Date(date);
     return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+}
+
+// 分页
+function changePage (page) {
+    $.ajax({
+        type: 'GET',
+        url: '/posts',
+        data: {
+            page: page
+        },
+        success: function (response) {
+            let html = template('postsTpl', response);
+            $('#postsBox').html(html);
+            
+            let page = template('pageTpl', response);
+            $('#page').html(page);
+        }
+    });
 }
