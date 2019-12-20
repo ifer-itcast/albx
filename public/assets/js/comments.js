b2025dc117ed7b87a12decfa3f953c43764282d2
ifer-itcast <yangkangkang@itcast.cn>
2019-12-20 15:25:18 +0800

    评论列表分页

--- public/assets/js/comments.js
+++ public/assets/js/comments.js
@@ -3,7 +3,30 @@ $.ajax({
     type: 'GET',
     url: '/comments',
     success: function (response) {
+        // 评论列表
         var html = template('commentsTpl', response);
         $('#commentsBox').html(html);
+        // 页码
+        var pageHtml = template('pageTpl', response);
+        $('#pageBox').html(pageHtml);
     }
 });
+
+
+function changePage(page) {
+    $.ajax({
+        type: 'GET',
+        url: '/comments',
+        data: {
+            page
+        },
+        success: function (response) {
+            // 评论列表
+            var html = template('commentsTpl', response);
+            $('#commentsBox').html(html);
+            // 页码
+            var pageHtml = template('pageTpl', response);
+            $('#pageBox').html(pageHtml);
+        }
+    });
+}
