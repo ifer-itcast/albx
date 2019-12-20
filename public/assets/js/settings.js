0a59d56f9b1e8bb5f2a42e3cbf4ab744786c1b17
ifer-itcast <yangkangkang@itcast.cn>
2019-12-20 18:16:58 +0800

    网站信息设置

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -12,6 +12,22 @@ $('#logo').on('change', function() {
         contentType: false,
         success: function (response) {
             $('#hiddenLogo').val(response[0].logo);
+            // 将 logo 图片显示在页面中
+            $('#preview').attr('src', response[0].logo);
         }
     });
+});
+
+$('#settingsForm').on('submit', function() {
+    var formData = $(this).serialize();
+    console.log(formData)
+    $.ajax({
+        type: 'POST',
+        url: '/settings',
+        data: formData,
+        success: function (response) {
+            location.reload();
+        }
+    });
+    return false;
 });
