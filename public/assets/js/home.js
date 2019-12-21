a3f3344944f1e495c6353cb106294bf368a6d9d9
ifer-itcast <yangkangkang@itcast.cn>
2019-12-21 21:46:09 +0800

    .

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -6,20 +6,19 @@ $.ajax({
 			data: response
 		});
 		$('#slidesBox').html(html);
+		var str = '<span></span>'.repeat(response.length-1);
+		$('.cursor').html('<span class="active"></span>' + str);
 
 		var swiper = Swipe(document.querySelector('.swipe'), {
 			auto: 3000,
 			transitionEnd: function(index) {
 				// index++;
-
 				$('.cursor span').eq(index).addClass('active').siblings('.active').removeClass('active');
 			}
 		});
-
 		// 上/下一张
 		$('.swipe .arrow').on('click', function() {
 			var _this = $(this);
-
 			if (_this.is('.prev')) {
 				swiper.prev();
 			} else if (_this.is('.next')) {
