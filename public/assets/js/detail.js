6085a9a430d77dbd26391f41781b1a18262d8d53
ifer-itcast <yangkangkang@itcast.cn>
2019-12-23 13:16:13 +0800

    评论功能

--- public/assets/js/detail.js
+++ public/assets/js/detail.js
@@ -1,5 +1,7 @@
 // 从地址栏中获取文章 ID
 var postId = getUrlParams('id');
+// 是否审核
+var review;
 
 $.ajax({
     type: 'GET',
@@ -20,16 +22,40 @@ $('#article').on('click', '#like', function() {
     });
 });
 
-// 获取网站配置信息
-$.ajax({
-    type: 'GET',
-    url: '/settings',
-    success: function (response) {
-        // 判断管理员是否开启了评论功能
-        if(response.comment) {
-            // 渲染评论
-            var html = template('commentTpl');
-            $('#comment').html(html);
+// 登录了才对评论进行处理
+if(isLogin) {
+    // 获取网站配置信息
+    $.ajax({
+        type: 'GET',
+        url: '/settings',
+        success: function (response) {
+            review = response.review;
+            // 判断管理员是否开启了评论功能
+            if(response.comment) {
+                // 渲染评论
+                var html = template('commentTpl');
+                $('#comment').html(html);
+            }
         }
-    }
+    });
+}
+
+// 评论功能
+$('#comment').on('submit', 'form', function() {
+    var content = $(this).find('textarea').val();
+    var userId = JSON.parse(localStorage.getItem('userInfo'))._id;
+    $.ajax({
+        type: 'POST',
+        url: '/comments',
+        data: {
+            author: userId,
+            post: postId,
+            content
+        },
+        success: function (response) {
+            alert('评论成功');
+            location.reload();
+        }
+    });
+    return false;
 });
